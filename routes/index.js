7883518997d9fce7951cb218e5d461670de51f95
Felipe Baro Tayahua <fbarotay@iu.edu>
2023-10-08 19:30:37 -0400

    Final commit completed

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,34 @@ router.get('/', async function(req, res, next) {
   res.render('index', { title: 'Dog House', dogs: dogs });
 });
 
+
+// Post a new dog
+router.post('/', (req, res) => {
+  console.log(req.body);
+
+  // Extract dog data from the request body
+  const { name, breed, age, gender, treat } = req.body;
+
+  // Create a new Dog instance with the extracted data
+  const newDog = new Dog({
+    name,
+    breed,
+    age,
+    gender,
+    treat,
+  });
+
+  // Save the new dog to the database
+  newDog
+    .save()
+    .then(() => {
+      console.log('Document Saved');
+      res.redirect('/'); // Redirect to the root route after saving
+    })
+    .catch((error) => {
+      console.error(error);
+      // Handle any errors here
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
+});
 module.exports = router;
